426005162b4b8c97a9f02e21a55c1bcf2870cffc
weiweilong <weiweilong_email@foxmail.com>
2016-07-19 11:08:01 +0800

    Initial Commit

---
+++ Chat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Chat
+//
+//  Created by 魏唯隆 on 16/7/19.
+//  Copyright © 2016年 魏唯隆. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
